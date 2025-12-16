455726fbf7e58a6026378a24bb69014dccc45ac6
temi42 <temitopeopemipo@gmail.com>
2025-12-16 19:43:08 +0000

    function handling %d/%i had more than 25 lines so i had to shorten it.

--- ft_putnbrint_fd.c
+++ ft_putnbrint_fd.c
@@ -6,7 +6,7 @@
 /*   By: tadeyelu <tadeyelu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 17:00:56 by tadeyelu          #+#    #+#             */
-/*   Updated: 2025/12/16 18:45:01 by tadeyelu         ###   ########.fr       */
+/*   Updated: 2025/12/16 19:41:38 by tadeyelu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,9 @@ int	ft_putnbrint_fd(int n, int fd)
 
 	count = 0;
 	if (n == -2147483648)
-	{
-		write(fd, "-2147483648", 11);
-		return (11);
-	}
+		return (write(fd, "-2147483648", 11));
 	if (n == 0)
-	{
-		write(fd, "0", 1);
-		return (1);
-	}
+		return (write(fd, "0", 1));
 	if (n >= -2147483647 && n <= -1)
 	{
 		write(fd, "-", 1);
